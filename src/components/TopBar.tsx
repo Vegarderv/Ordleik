a0c9d9fbe752eb2956b52de8d55d900b7d960a1f
Vegard Ervik <vegarderv@gmail.com>
2022-02-06 15:49:37 +0100

    working with statistics

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { newWord } from "../redux/word/wordAction"; 
+import { newWord } from "../redux/word/wordAction";
 import getRandomLine from "../utils/wordchooser";
+import * as Icon from "react-bootstrap-icons";
+import { Modal } from "react-bootstrap";
 
 const TopBar = () => {
   const dispatch = useDispatch();
 
-  dispatch(newWord(getRandomLine()))
+  dispatch(newWord(getRandomLine()));
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <div className="top-bar">
-        <h2>ORDLEIK</h2>
+        <div></div>
+        <div>
+          <h2>ORDLEIK</h2>
+        </div>
+        <div>
+          <Icon.BarChart className="stats" onClick={() => {handleShow()}}></Icon.BarChart>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Woohoo, you're reading this text in a modal!
+            </Modal.Body>
+            <Modal.Footer>
+       
+            </Modal.Footer>
+          </Modal>
+        </div>
       </div>
     </>
   );
